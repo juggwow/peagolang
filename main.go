af9083428508aa5670ed692d6503997102bd43a9
patna <p.wiriyar@gmail.com>
2023-06-02 05:51:43 +0700

    say hello

--- main.go
+++ main.go
@@ -55,7 +55,7 @@ func Gettest() gin.HandlerFunc {
 	return func(c *gin.Context) {
 
 		c.IndentedJSON(http.StatusOK, gin.H{
-			"token": "Hello Hello Hello",
+			"say": "Hello Hello Hello 55555",
 		})
 
 	}
