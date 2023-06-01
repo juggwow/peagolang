0e8a50a460ed7f8f4064c3540ecaccd2caf48dbb
patna <p.wiriyar@gmail.com>
2023-06-01 20:10:36 +0700

    edit test path

--- main.go
+++ main.go
@@ -55,7 +55,7 @@ func Gettest() gin.HandlerFunc {
 	return func(c *gin.Context) {
 
 		c.IndentedJSON(http.StatusOK, gin.H{
-			"token": "Hello world form server",
+			"token": "Hello Hello",
 		})
 
 	}
